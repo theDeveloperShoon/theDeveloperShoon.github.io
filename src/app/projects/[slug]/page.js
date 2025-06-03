037a92ac3a64f2e29553ffa8869f539d75007325
Sean Cotter <Seancotter04@gmail.com>
2025-06-03 16:39:47 -0400

    Added RepoPage

---
+++ src/app/projects/[slug]/page.js
@@ -0,0 +1,14 @@
+import { Project } from '../../components/projects';
+
+export async function generateStaticParams() {
+  const res = await fetch('https://api.github.com/users/theDeveloperShoon/repos');
+  const repos = await res.json();
+  return repos.map(repo => ({ slug: repo.name }));
+}
+
+export default async function RepoPage({ params }) {
+  const res = await fetch(`https://api.github.com/repos/theDeveloperShoon/${params.slug}`);
+  const repo = await res.json();
+
+  return <Project repo={repo} />;
+}
