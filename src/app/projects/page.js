06eb851c25a48cd1faf327926693add1392bab86
Sean Cotter <Seancotter04@gmail.com>
2025-06-03 16:39:38 -0400

    Added projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,13 +1,16 @@
+import { Projects } from "../components/projects";
 
-export default function Page() {
-  return (
-    <section>
-        <div className="flex flex-col items-center justify-center min-h-screen p-4">
-            <h1 className="text-4xl font-bold mb-4">Welcome to My Projects Page</h1>
-            <p className="text-lg mb-2">This is a simple page built with Next.js.</p>
-            <p className="text-lg mb-2">Feel free to explore and learn more about me.</p>
-            <p className="text-lg mb-2">Check out the links below for more information.</p>
-        </div>
-    </section>
-  );
+export default async function Page() {
+  const res = await fetch('https://api.github.com/users/theDeveloperShoon/repos', { next: { revalidate: 3600 } });
+  const repos = await res.json();
+  
+  return <Projects repos={repos} />;
 }
+// <section>
+//     <div className="flex flex-col items-center justify-center min-h-screen p-4">
+//         <h1 className="text-4xl font-bold mb-4">Welcome to My Projects Page</h1>
+//         <p className="text-lg mb-2">This is a simple page built with Next.js.</p>
+//         <p className="text-lg mb-2">Feel free to explore and learn more about me.</p>
+//         <p className="text-lg mb-2">Check out the links below for more information.</p>
+//     </div>
+// </section>
