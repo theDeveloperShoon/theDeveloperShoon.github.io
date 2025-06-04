4de08762073b4364c82596182703ce140c4fdc42
Sean Cotter <Seancotter04@gmail.com>
2025-06-04 15:43:20 -0400

    Modified NavBar to have hover color and space from the top

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -18,12 +18,12 @@ const navItems = {
 export default function Navbar() {
     return (
         <header className="z-[999] relative">
-            <nav className="flex fixed left-1/2 h-16 -translate-x-1/2 px-4 bg-white/80 backdrop-blur-md shadow-md rounded-full">
+            <nav className="flex fixed top-[0.15rem] left-1/2 h-16 -translate-x-1/2 px-4 bg-white/80 backdrop-blur-md shadow-md rounded-full">
                 <div className="flex items-center justify-center gap-6 ">
                     {Object.entries(navItems).map(([path, { name }]) => {
                         return (
                             <Link
-                                className="flex w-full items-center justify-center text-white hover:text-gray-950"
+                                className="flex w-full items-center justify-center text-white hover:bg-gray-300 rounded-full px-2"
                                 key={path}
                                 href={path}
                                 >
