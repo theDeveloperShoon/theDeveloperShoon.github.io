65453081f97dd72547c90f7ce25aa8cf43904b58
Sean Cotter <Seancotter04@gmail.com>
2025-06-05 00:26:46 -0400

    Removed commented code

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -6,11 +6,3 @@ export default async function Page() {
   
   return <Projects repos={repos} />;
 }
-// <section>
-//     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-//         <h1 className="text-4xl font-bold mb-4">Welcome to My Projects Page</h1>
-//         <p className="text-lg mb-2">This is a simple page built with Next.js.</p>
-//         <p className="text-lg mb-2">Feel free to explore and learn more about me.</p>
-//         <p className="text-lg mb-2">Check out the links below for more information.</p>
-//     </div>
-// </section>
